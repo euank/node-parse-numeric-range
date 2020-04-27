11f389e01afbd83f1aada8851ffdcec3d3bc57ef
Sahin Deniz <seahindeniz@gmail.com>
2020-04-27 12:34:48 +0300

    Common and ES6 fixes

--- index.js
+++ index.js
@@ -2,27 +2,35 @@
  * @param {string} string    The string to parse
  * @returns {Array<number>}  Returns an energetic array.
  */
-export default function parsePart (string) {
-  let res = [], m
-  for (let str of string.split(',').map(str => str.trim())) {
+function parsePart(string) {
+  let res = [];
+  let m;
+
+  for (let str of string.split(",").map((str) => str.trim())) {
     // just a number
     if (/^-?\d+$/.test(str)) {
-      res.push(parseInt(str, 10))
-    } else if ((m = str.match(/^(-?\d+)(-|\.\.\.?|\u2025|\u2026|\u22EF)(-?\d+)$/))) {
-      // 1-5 or 1..5 (equivilant) or 1...5 (doesn't include 5)
-      let [_, lhs, sep, rhs] = m
+      res.push(parseInt(str, 10));
+    } else if (
+      (m = str.match(/^(-?\d+)(-|\.\.\.?|\u2025|\u2026|\u22EF)(-?\d+)$/))
+    ) {
+      // 1-5 or 1..5 (equivalent) or 1...5 (doesn't include 5)
+      let [_, lhs, sep, rhs] = m;
 
       if (lhs && rhs) {
-        lhs = parseInt(lhs)
-        rhs = parseInt(rhs)
-        const incr = lhs < rhs ? 1 : -1
+        lhs = parseInt(lhs);
+        rhs = parseInt(rhs);
+        const incr = lhs < rhs ? 1 : -1;
 
         // Make it inclusive by moving the right 'stop-point' away by one.
-        if (sep === '-' || sep === '..' || sep === '\u2025') rhs += incr
+        if (sep === "-" || sep === ".." || sep === "\u2025") rhs += incr;
 
-        for (let i = lhs; i !== rhs; i += incr) res.push(i)
+        for (let i = lhs; i !== rhs; i += incr) res.push(i);
       }
     }
   }
-  return res
+
+  return res;
 }
+
+exports.default = parsePart;
+module.exports = parsePart;
